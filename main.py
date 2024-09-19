bc617e30bae84aa0ff6026adb2faab2d23228908
jagaldol <jagaldol.dev@gmail.com>
2024-09-19 16:24:02 +0000

    feat: Add space esacpe

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ app.add_middleware(
 # 요청 바디에서 받을 데이터 모델 정의
 class RequestBody(BaseModel):
     query: str  # 필수 인자
-    history: Optional[list[str]] = None,
+    history: Optional[list[str]] = None
     rt_alpha: Optional[float] = 0.95
     rt_top_k: Optional[int] = 3 
     rt_min_score: Optional[float] = 0.55
@@ -50,10 +50,7 @@ class RequestBody(BaseModel):
 async def run_inference(body: RequestBody):
     # 요청에서 query와 hyperparameters를 추출
     query = body.query
-    if body.history is None:
-        history = []
-    else:
-        history = body.history
+    history = body.history if body.history is not None else []
     retriever_kwargs = {
         "alpha": body.rt_alpha,
         "top_k": body.rt_top_k,
@@ -78,54 +75,11 @@ async def run_inference(body: RequestBody):
     def word_streamer():
         for word in response:  # response가 generator일 때 사용
             escaped_word = word.replace('\n', '<enter_token>')
+            escaped_word = escaped_word.replace(' ', '<space_token>')
             yield f"data: {escaped_word}\n\n"  # SSE 포맷 유지 또는 필요시 수정
-        yield "data: <stream_end_token>\n\n"
-
     
     return StreamingResponse(word_streamer(), media_type="text/event-stream")
 
-
-
-@app.get("/inference")
-async def run_inference(
-    query: str,
-    rt_alpha: Optional[float] = 0.95,
-    rt_top_k: Optional[int] = 3,
-    rt_min_score: Optional[float] = 0.55,
-    lm_temperature: Optional[float] = 0.5,
-    lm_top_p: Optional[float] = 0.7,
-    lm_repetition_penalty: Optional[float] = 1.1
-):
-    # 요청에서 query와 hyperparameters를 추출
-    query = query
-    retriever_kwargs = {
-        "alpha": rt_alpha,
-        "top_k": rt_top_k,
-        "min_score": rt_min_score
-    }
-
-    llm_kwargs = {
-        "temperature": lm_temperature,
-        "top_p": lm_top_p,
-        "repetition_penalty": lm_repetition_penalty
-    }
-
-    # chaining 함수 호출 및 generator 생성
-    response = chain["rag chain"].chaining(
-        query,
-        retriever_kwargs=retriever_kwargs,
-        llm_kwargs=llm_kwargs
-    )
-
-    # 단어별로 데이터를 스트리밍하기 위한 generator
-    def word_streamer():
-        for word in response:  # response가 generator일 때 사용
-            escaped_word = word.replace('\n', '<enter_token>')
-            yield f"data: {escaped_word}\n\n"  # SSE 포맷 유지 또는 필요시 수정
-        yield "data: <stream_end_token>\n\n"
-
-    
-    return StreamingResponse(word_streamer(), media_type="text/event-stream")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0")
