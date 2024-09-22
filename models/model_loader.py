e4d6d679d49897cdd9ef0885445b63d5557d1b1d
jagaldol <jagaldol.dev@gmail.com>
2024-09-22 08:19:54 +0000

    feat:Apply Quantization(14GB -> 5GB used)

--- models/model_loader.py
+++ models/model_loader.py
@@ -1,9 +1,21 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from FlagEmbedding import BGEM3FlagModel
 
 def load_gemma_model(model_id: str):
+
+    bnb_config = BitsAndBytesConfig(
+        # 입력값을 4 bit로 변환
+        load_in_4bit=True,
+        # 모델을 4 bit으로 양자화
+        bnb_4bit_quant_type="nf4",
+        # 4 bit 계산에 사용될 데이터 유형, 4비트 부동소수점(bfloat16), 4비트 정수(uint8)
+        bnb_4bit_compute_dtype=torch.float16
+)
+
+
     """모델과 토크나이저 로드"""
-    model = AutoModelForCausalLM.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config)
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     return model, tokenizer
 
